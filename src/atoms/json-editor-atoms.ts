addc318796334eb0e0897a8a3c61fe32bd43ab6c
Tim Eichholz <tim.eichholz98@web.de>
2025-04-06 17:31:49 +0200

    style: Refactor JSON editor atoms with consistent formatting

--- src/atoms/json-editor-atoms.ts
+++ src/atoms/json-editor-atoms.ts
@@ -1,10 +1,10 @@
-import { atom } from 'jotai';
+import { atom } from "jotai";
 
 // Define the type for string changes
 export type StringChange = {
-  path: string;
-  value: string;
-  timestamp: number;
+	path: string;
+	value: string;
+	timestamp: number;
 };
 
 // Create an atom to store the history of string changes
@@ -12,12 +12,12 @@ export const stringChangesAtom = atom<StringChange[]>([]);
 
 // Atom to add a new change to the history
 export const addStringChangeAtom = atom(
-  (get) => get(stringChangesAtom),
-  (get, set, newChange: Omit<StringChange, 'timestamp'>) => {
-    const change: StringChange = {
-      ...newChange,
-      timestamp: Date.now(),
-    };
-    set(stringChangesAtom, [...get(stringChangesAtom), change]);
-  }
+	(get) => get(stringChangesAtom),
+	(get, set, newChange: Omit<StringChange, "timestamp">) => {
+		const change: StringChange = {
+			...newChange,
+			timestamp: Date.now(),
+		};
+		set(stringChangesAtom, [...get(stringChangesAtom), change]);
+	},
 );
